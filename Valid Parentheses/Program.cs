e2c84cbebd1aa7110e2980aeaeffcf24131878e1
Bita Rahimkhani <taaghche.bitarahimkhani@gmail.com>
2023-04-04 00:28:37 +0430

    Add project files.

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+IsValid("()[]{}");
+bool IsValid(string s)
+{
+    if(s.Length%2!=0)
+    {
+        return false;
+    }
+    if (s[0] == ')' || s[0]==']'|| s[0]=='}')
+    {
+        return false;
+    }
+    Stack<char> full = new Stack<char>();
+    var tmp =new char();
+    for (int i = 0; i < s.Length; i++)
+    {
+
+        switch (s[i])
+        {
+            case '(':
+                full.Push(s[i]);
+                break;
+            case ')':
+                if (full.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    tmp=full.Pop();
+                    if(tmp=='{'||tmp=='[')
+                    {
+                        return false;
+                    }
+                }
+                break;
+            case '[':
+                full.Push(s[i]);
+                break;
+            case ']':
+                if (full.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    tmp = full.Pop();
+                    if (tmp == '{' || tmp == '(')
+                    {
+                        return false;
+                    }
+                }
+                break;
+
+            case '{':
+                full.Push(s[i]);
+                break;
+
+            case '}':
+                if (full.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    tmp = full.Pop();
+                    if (tmp == '[' || tmp == '(')
+                    {
+                        return false;
+                    }
+                }
+                break;
+
+        }
+    }
+    if(full.Count != 0)
+    {
+        return false;
+    }
+    return true;
+}
